e921aa3399a048b167fbdb7a87385687c810a215
Dmitry Mozyrchuk <dmitry.mozyrchuk@mainsoft.by>
2017-08-15 10:50:32 +0300

    Refactoring

--- KSTokenView/KSUtils.swift
+++ KSTokenView/KSUtils.swift
@@ -27,36 +27,36 @@ import UIKit
 let KSTextEmpty = "\u{200B}"
 
 class KSUtils : NSObject {
-   
-   class func getRect(_ str: NSString, width: CGFloat, height: CGFloat, font: UIFont) -> CGRect {
-      let rectangleStyle = NSMutableParagraphStyle.default.mutableCopy() as! NSMutableParagraphStyle
-      rectangleStyle.alignment = NSTextAlignment.center
-      let rectangleFontAttributes = [NSFontAttributeName: font, NSParagraphStyleAttributeName: rectangleStyle]
-      return str.boundingRect(with: CGSize(width: width, height: height), options: NSStringDrawingOptions.usesLineFragmentOrigin, attributes: rectangleFontAttributes, context: nil)
-   }
-   
-   class func getRect(_ str: NSString, width: CGFloat, font: UIFont) -> CGRect {
-      let rectangleStyle = NSMutableParagraphStyle.default.mutableCopy() as! NSMutableParagraphStyle
-      rectangleStyle.alignment = NSTextAlignment.center
-      let rectangleFontAttributes = [NSFontAttributeName: font, NSParagraphStyleAttributeName: rectangleStyle]
-      return str.boundingRect(with: CGSize(width: width, height: CGFloat(MAXFLOAT)), options: NSStringDrawingOptions.usesLineFragmentOrigin, attributes: rectangleFontAttributes, context: nil)
-   }
-   
-   class func widthOfString(_ str: String, font: UIFont) -> CGFloat {
-      let attrs = [NSFontAttributeName: font]
-      let attributedString = NSMutableAttributedString(string:str, attributes:attrs)
-      return attributedString.size().width
-   }
-   
+
+    class func getRect(_ str: NSString, width: CGFloat, height: CGFloat, font: UIFont) -> CGRect {
+        let rectangleStyle = NSMutableParagraphStyle.default.mutableCopy() as! NSMutableParagraphStyle
+        rectangleStyle.alignment = NSTextAlignment.center
+        let rectangleFontAttributes = [NSFontAttributeName: font, NSParagraphStyleAttributeName: rectangleStyle]
+        return str.boundingRect(with: CGSize(width: width, height: height), options: NSStringDrawingOptions.usesLineFragmentOrigin, attributes: rectangleFontAttributes, context: nil)
+    }
+
+    class func getRect(_ str: NSString, width: CGFloat, font: UIFont) -> CGRect {
+        let rectangleStyle = NSMutableParagraphStyle.default.mutableCopy() as! NSMutableParagraphStyle
+        rectangleStyle.alignment = NSTextAlignment.center
+        let rectangleFontAttributes = [NSFontAttributeName: font, NSParagraphStyleAttributeName: rectangleStyle]
+        return str.boundingRect(with: CGSize(width: width, height: CGFloat(MAXFLOAT)), options: NSStringDrawingOptions.usesLineFragmentOrigin, attributes: rectangleFontAttributes, context: nil)
+    }
+
+    class func widthOfString(_ str: String, font: UIFont) -> CGFloat {
+        let attrs = [NSFontAttributeName: font]
+        let attributedString = NSMutableAttributedString(string:str, attributes:attrs)
+        return attributedString.size().width
+    }
+
 }
 
 extension UIColor {
-   func darkendColor(_ darkRatio: CGFloat) -> UIColor {
-      var h: CGFloat = 0.0, s: CGFloat = 0.0, b: CGFloat = 0.0, a: CGFloat = 0.0
-      if (getHue(&h, saturation: &s, brightness: &b, alpha: &a)) {
-         return UIColor(hue: h, saturation: s, brightness: b*darkRatio, alpha: a)
-      } else {
-         return self
-      }
-   }
+    func darkendColor(_ darkRatio: CGFloat) -> UIColor {
+        var h: CGFloat = 0.0, s: CGFloat = 0.0, b: CGFloat = 0.0, a: CGFloat = 0.0
+        if (getHue(&h, saturation: &s, brightness: &b, alpha: &a)) {
+            return UIColor(hue: h, saturation: s, brightness: b*darkRatio, alpha: a)
+        } else {
+            return self
+        }
+    }
 }
